6f9925307928d7b40b9a32d632b4760db9002bc0
Ramiro Mendez <zapedax@gmail.com>
2020-04-06 20:00:11 -0500

    Login

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SSPISW - General</title>
     <!-- Icono pestaña -->
     <link rel="icon" type="image/png" href="./img/icon.png">
     <!-- Relacion archivo(online) bulma-css -->
